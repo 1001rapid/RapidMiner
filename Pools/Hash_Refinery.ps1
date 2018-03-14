6b5bc5b56bcd5aed78b5b1f68ed815d7f1cae39c
1001rapid <34758417+1001rapid@users.noreply.github.com>
2018-03-15 01:13:56 +0300

    Add files via upload

---
+++ Pools/Hash_Refinery.ps1
@@ -0,0 +1,129 @@
+﻿param(
+    [Parameter(Mandatory = $true)]
+    [String]$Querymode = $null,
+    [Parameter(Mandatory = $false)]
+    [pscustomobject]$Info
+    )
+
+#. .\Include.ps1
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+$ActiveOnManualMode    = $false
+$ActiveOnAutomaticMode = $true
+$ActiveOnAutomatic24hMode = $true
+$AbbName= 'H.RFRY'
+$WalletMode='WALLET'
+$Result=@()
+
+
+
+#****************************************************************************************************************************************************************************************
+#****************************************************************************************************************************************************************************************
+#****************************************************************************************************************************************************************************************
+
+
+
+if ($Querymode -eq "info"){
+    $Result= [PSCustomObject]@{
+                    Disclaimer = "Autoexchange to @@currency coin specified in config.txt, no registration required"
+                    ActiveOnManualMode=$ActiveOnManualMode  
+                    ActiveOnAutomaticMode=$ActiveOnAutomaticMode
+                    ActiveOnAutomatic24hMode=$ActiveOnAutomatic24hMode
+                    AbbName=$AbbName
+                    WalletMode=$WalletMode
+                         }
+    }
+
+
+
+
+#****************************************************************************************************************************************************************************************
+#****************************************************************************************************************************************************************************************
+#****************************************************************************************************************************************************************************************
+
+
+    if ($Querymode -eq "wallet")    {
+        
+                            
+                            try {
+                                $http="http://pool.hashrefinery.com/api/wallet?address="+$Info.user
+                                $HR_Request = Invoke-WebRequest $http -UseBasicParsing -timeoutsec 10 | ConvertFrom-Json 
+                            }
+                            catch {}
+        
+        
+                            if ($HR_Request -ne $null -and $HR_Request -ne ""){
+                                $Result=  [PSCustomObject]@{
+                                                        Pool =$name
+                                                        currency = $HR_Request.currency
+                                                        balance = $HR_Request.balance
+                                                    }
+                                    }
+
+                        remove-variable HR_Request                                    
+                        }
+
+
+
+
+#****************************************************************************************************************************************************************************************
+#****************************************************************************************************************************************************************************************
+#****************************************************************************************************************************************************************************************
+
+
+if (($Querymode -eq "core" ) -or ($Querymode -eq "Menu")){
+
+        try
+        {
+            $HR_Request = Invoke-WebRequest "http://pool.hashrefinery.com/api/status" -UseBasicParsing | ConvertFrom-Json
+        }
+        catch
+        {
+            EXIT
+        }
+
+
+
+        $HR_Request | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+
+                        $Divisor = (Get-Algo-Divisor $_) / 1000
+                    
+
+                
+                $Result += [PSCustomObject]@{
+                                Algorithm =  get-algo-unified-name $_
+                                Info = $null
+                                Price = [Double]$HR_Request.$_.estimate_current/$Divisor
+                                Price24h =[Double]$HR_Request.$_.estimate_last24h/$Divisor
+                                Protocol = "stratum+tcp"
+                                Host = $_+".us.hashrefinery.com"
+                                Port = $HR_Request.$_.port
+                                User = $CoinsWallets.get_item($currency)
+                                Pass = "c=$Currency,#WorkerName#"
+                                Location = "US"
+                                SSL = $false
+                                AbbName = $AbbName
+                                ActiveOnManualMode    = $ActiveOnManualMode
+                                ActiveOnAutomaticMode = $ActiveOnAutomaticMode
+                                PoolWorkers = $HR_Request.$_.workers
+                                WalletMode=$WalletMode
+                                WalletSymbol    = $currency
+                                PoolName = $Name
+                                Fee = $HR_Request.$_.Fees/100
+                    
+                }
+            
+                
+        }
+
+
+}
+
+
+#****************************************************************************************************************************************************************************************
+#****************************************************************************************************************************************************************************************
+#****************************************************************************************************************************************************************************************
+
+
+$Result |ConvertTo-Json | Set-Content ("$name.tmp")
+Remove-variable Result
---
+++ Pools/SUPRNOVA.ps1
@@ -0,0 +1,147 @@
+param(
+    [Parameter(Mandatory = $false)]
+    [String]$Querymode = $null,
+    [Parameter(Mandatory = $false)]
+    [pscustomobject]$Info
+    )
+
+#. .\Include.ps1
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+$ActiveOnManualMode    = $true
+$ActiveOnAutomaticMode = $false
+$ActiveOnAutomatic24hMode=$false
+$AbbName='SNV'
+$WalletMode="APIKEY"
+$Result=@()
+
+if ($Querymode -eq "info"){
+    $Result = [PSCustomObject]@{
+                    Disclaimer = "Must register and set wallet for each coin on web"
+                    ActiveOnManualMode=$ActiveOnManualMode  
+                    ActiveOnAutomaticMode=$ActiveOnAutomaticMode
+                    ActiveOnAutomatic24hMode=$ActiveOnAutomaticMode
+                    ApiData = $true
+                    AbbName=$AbbName
+                    WalletMode=$WalletMode
+                          }
+    }
+
+
+    if ($Querymode -eq "APIKEY")    {
+        
+         
+
+                             
+                            Switch($Info.Symbol) {
+                                "DGB" {$Info.Symbol=$Info.Symbol+($Info.Algorithm.substring(0,1))}
+                               }
+
+                        
+                            try {
+                                $http="http://"+$Info.Symbol+".suprnova.cc/index.php?page=api&action=getuserbalance&api_key="+$Info.ApiKey+"&id="
+                                #$http |write-host  
+                                [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12                              
+                                $Suprnova_Request =  Invoke-WebRequest $http -UseBasicParsing -timeoutsec 5
+                                $Suprnova_Request = $Suprnova_Request | ConvertFrom-Json | Select-Object -ExpandProperty getuserbalance | Select-Object -ExpandProperty data
+                                }
+                            catch {
+                                  }
+        
+        
+                        
+                                $Result=[PSCustomObject]@{
+                                                        Pool =$name
+                                                        currency = $Info.OriginalCoin
+                                                        balance = $Suprnova_Request.confirmed+$Suprnova_Request.unconfirmed
+                                                    }
+                         
+                         
+                                                
+                        }
+
+                        
+
+
+
+if (($Querymode -eq "core" ) -or ($Querymode -eq "Menu")){
+
+    
+        $Pools=@()
+        $Pools +=[pscustomobject]@{"coin" = "DECRED"; "algo"="Blake14r"; "symbol"= "DCR"; "server"="dcr.suprnova.cc";"port"="3252";"location"="US"}
+        $Pools +=[pscustomobject]@{"coin" = "BITCORE"; "algo"="BITCORE"; "symbol"= "BTX"; "server"="btx.suprnova.cc";"port"="3629";"location"="US"}
+        $Pools +=[pscustomobject]@{"coin" = "DIGIBYTE";"algo"="SKEIN"; "symbol"= "DGB";"server"="dgbs.suprnova.cc"; "port"= "5226";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "DIGIBYTE";"algo"="myriad-groestl"; "symbol"= "DGB";"server"="dgbg.suprnova.cc"; "port"= "7978";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "HUSH";"algo"="Equihash"; "symbol"= "HUSH";"server"="hush.suprnova.cc"; "port"= "4048";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "LBRY";"algo"="LBRY"; "symbol"= "LBC";"server"="lbry.suprnova.cc"; "port"= "6256";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "MONACOIN";"algo"="lyra2v2"; "symbol"= "MONA";"server"="mona.suprnova.cc"; "port"= "2995";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "SIGNATUM";"algo"="SKUNK"; "symbol"= "SIGT";"server"="sigt.suprnova.cc"; "port"= "7106";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "VELTOR";"algo"="VELTOR"; "symbol"= "VLT";"server"="veltor.suprnova.cc"; "port"= "8897";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "ZENCASH";"algo"="Equihash"; "symbol"= "ZEN";"server"="zen.suprnova.cc"; "port"= "3618";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "ZCASH";"algo"="Equihash"; "symbol"= "ZEC";"server"="zec-us.suprnova.cc"; "port"= "2142";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "ZCASH";"algo"="Equihash"; "symbol"= "ZEC";"server"="zec-eu.suprnova.cc"; "port"= "2142";"location"="EUROPE"};
+        $Pools +=[pscustomobject]@{"coin" = "ZCASH";"algo"="Equihash"; "symbol"= "ZEC";"server"="zec-apac.suprnova.cc"; "port"= "2142";"location"="ASIA"};
+        $Pools +=[pscustomobject]@{"coin" = "ZCOIN";"algo"="LYRA2Z"; "symbol"= "XZC";"server"="xzc.suprnova.cc"; "port"= "1569";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "ZCOIN";"algo"="LYRA2Z"; "symbol"= "XZC";"server"="xzc.suprnova.cc"; "port"= "1569";"location"="EUROPE"};
+        $Pools +=[pscustomobject]@{"coin" = "ZCOIN";"algo"="LYRA2Z"; "symbol"= "XZC";"server"="xzc-apac.suprnova.cc"; "port"= "1569";"location"="ASIA"};
+        $Pools +=[pscustomobject]@{"coin" = "DASHCOIN";"algo"="X11"; "symbol"= "DASH";"server"="dash.suprnova.cc"; "port"= "9995";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "ZCLASSIC";"algo"="Equihash"; "symbol"= "ZCL";"server"="zcl.suprnova.cc"; "port"= "4042";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "ZCLASSIC";"algo"="Equihash"; "symbol"= "ZCL";"server"="zcl-apac.suprnova.cc"; "port"= "4042";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "KOMODO";"algo"="Equihash"; "symbol"= "KMD";"server"="kmd.suprnova.cc"; "port"= "6250";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "MONERO";"algo"="CRYPTONIGHT"; "symbol"= "XMR";"server"="xmr-eu.suprnova.cc"; "port"= "5222";"location"="EU"};
+        $Pools +=[pscustomobject]@{"coin" = "CHAINCOIN";"algo"="C11"; "symbol"= "CHC";"server"="chc.suprnova.cc"; "port"= "5888";"location"="EU"};
+        $Pools +=[pscustomobject]@{"coin" = "ETHEREUM";"algo"="ETHASH"; "symbol"= "ETH";"server"="eth.suprnova.cc"; "port"= "5000";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "SIBCOIN";"algo"="X11gost"; "symbol"= "ETH";"server"="sib.suprnova.cc"; "port"= "3458";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "UBIQ";"algo"="Ethash"; "symbol"= "UBQ";"server"="ubiq.suprnova.cc"; "port"= "3030";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "EXPANSE";"algo"="Ethash"; "symbol"= "EXP";"server"="exp.suprnova.cc"; "port"= "3333";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "ELECTRONEUM";"algo"="CRYPTONIGHT"; "symbol"= "ETN";"server"="etn.suprnova.cc"; "port"= "8875";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "SMARTCASH";"algo"="keccak"; "symbol"= "SMART";"server"="smart.suprnova.cc"; "port"= "4192";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "BITCOINZ";"algo"="equihash"; "symbol"= "BTCZ";"server"="btcz.suprnova.cc"; "port"= "5586";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "BITCOINGOLD";"algo"="equihash"; "symbol"= "BTG";"server"="btg.suprnova.cc"; "port"= "8816";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "polytimos";"algo"="polytimos"; "symbol"= "POLY";"server"="poly.suprnova.cc"; "port"= "7935";"location"="US"};
+        $Pools +=[pscustomobject]@{"coin" = "Straks";"algo"="lyra2v2"; "symbol"= "STAK";"server"="stak.suprnova.cc"; "port"= "7706";"location"="US"};
+
+        
+      
+        
+
+        $Pools |ForEach-Object {
+
+ 
+
+                            $Result+=[PSCustomObject]@{
+                                    Algorithm     = $_.Algo
+                                    Info          = $_.Coin
+                                    Price         = $null
+                                    Price24h      = $null
+                                    Protocol      = "stratum+tcp"
+                                    Host          = $_.Server
+                                    Port          = $_.Port
+                                    User          = "$Username.$WorkerName"
+                                    Pass          = "x"
+                                    Location      = $_.Location
+                                    SSL           = $false
+                                    Symbol        = $_.symbol
+                                    AbbName       = $AbbName
+                                    ActiveOnManualMode    = $ActiveOnManualMode
+                                    ActiveOnAutomaticMode = $ActiveOnAutomaticMode
+                                    PoolWorkers     = $Null
+                                    PoolHashRate    = [double]$ApiResponse.hashrate
+                                    PoolName        = $Name
+                                    WalletMode      = $WalletMode
+                                    WalletSymbol= $_.Coin #not symbol available
+                                    OriginalAlgorithm =  $_.Algo
+                                    OriginalCoin = $_.Coin
+                                    Fee = 0.01
+                                    EthStMode = 3
+
+
+                                }
+
+                        }
+              
+                Remove-Variable Pools
+        }
+                  
+$Result |ConvertTo-Json | Set-Content ("$name.tmp")
+Remove-Variable Result
